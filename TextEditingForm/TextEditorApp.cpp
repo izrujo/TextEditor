80384eca6ddc51cc1d054dc4e9e06ee599e9bc0e
ChanHyung Kim <kch7990@gmail.com>
2021-02-04 08:37:38 +0900

    Change Window name

--- TextEditingForm/TextEditorApp.cpp
+++ TextEditingForm/TextEditorApp.cpp
@@ -4,7 +4,7 @@
 BOOL TextEditorApp::InitInstance() {
 	TextEditingForm *textEditingForm = new TextEditingForm;
 
-	textEditingForm->CreateEx(NULL, AfxRegisterWndClass(NULL), "Basic Text Editor", WS_POPUP,
+	textEditingForm->CreateEx(NULL, AfxRegisterWndClass(NULL), "CNTextEditor", WS_POPUP,
 		CRect(200, 200, 500, 500), NULL, NULL, NULL);
 	textEditingForm->ShowWindow(SW_SHOW);
 	textEditingForm->UpdateWindow();
